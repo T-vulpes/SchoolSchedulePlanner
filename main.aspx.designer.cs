19b1d322175b9e3fecb98386f7d3d59927edb8f3
Deniz <167799743+T-vulpes@users.noreply.github.com>
2024-07-29 16:54:42 +0300

    main.aspx.designer.cs

--- main.aspx.designer.cs
+++ main.aspx.designer.cs
@@ -1,98 +1,15 @@
-﻿//------------------------------------------------------------------------------
-// <otomatik üretildi>
-//     Bu kod bir araç tarafından oluşturuldu.
-//
-//     Bu dosyada yapılacak değişiklikler yanlış davranışa neden olabilir ve
-//     kod tekrar üretildi. 
-// </otomatik üretildi>
-//------------------------------------------------------------------------------
-
 namespace courseschedule
 {
-
-
     public partial class main
-    {
-
-        /// <summary>
-        /// form1 denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
+    { 
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-
-        /// <summary>
-        /// EducationLevel denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.DropDownList EducationLevel;
-
-        /// <summary>
-        /// Year denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox Year;
-
-        /// <summary>
-        /// Courses denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.TextBox Courses;
-
-        /// <summary>
-        /// SubmitButton denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlButton SubmitButton;
-
-        /// <summary>
-        /// ClearButton denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList EducationLevel;   
+        protected global::System.Web.UI.WebControls.TextBox Year;   
+        protected global::System.Web.UI.WebControls.TextBox Courses;   
+        protected global::System.Web.UI.HtmlControls.HtmlButton SubmitButton;    
         protected global::System.Web.UI.HtmlControls.HtmlButton ClearButton;
-
-        /// <summary>
-        /// SchedulePanel denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel SchedulePanel;
-
-        /// <summary>
-        /// ScheduleTable denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
+        protected global::System.Web.UI.WebControls.Panel SchedulePanel;  
         protected global::System.Web.UI.WebControls.Table ScheduleTable;
-
-        /// <summary>
-        /// DownloadButton denetimi.
-        /// </summary>
-        /// <remarks>
-        /// Otomatik üretilmiş alan.
-        /// Değiştirmek için, alan bildirimini tasarımcı dosyasından arka plan kod dosyasına taşıyın.
-        /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlButton DownloadButton;
     }
 }
